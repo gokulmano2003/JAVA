fba5a9e796cc54dc51cdac1a21f51176f27322c6
gokulmano2003 <121616386+gokulmano2003@users.noreply.github.com>
2024-05-27 20:02:31 +0530

    Create exDay1.java

---
+++ exDay1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Task;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class NewClass {
+     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("name pls!!!!!");
+        String name=sc.next();
+        System.out.print(name);// TODO code application logic here
+    }
+}

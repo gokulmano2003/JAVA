0e982a58dbf4a2818ab5133a24b0f8cfcb8417ea
gokulmano2003 <121616386+gokulmano2003@users.noreply.github.com>
2024-05-27 20:00:11 +0530

    Create exDay2.java

---
+++ exDay2.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+//class name{
+//    public int rename(int a,int b){
+//    a+=b;
+//    return a;
+//}
+//}
+class cbe {
+
+    public String puram(String a, String b) {
+        a+= b;
+        return a;
+       
+    }
+}
+
+public class argument {
+//    public static void main(String[] gok){
+//        name me=new name();
+//       int bs= me.rename(30,30);
+//        System.out.println(bs);
+//      final  String passmark="35";
+//        System.out.println(passmark);
+//        String  end="45";     
+//        System.out.println(passmark);
+//    }
+
+    public static void main(String[] arg) {
+
+        cbe as = new cbe();
+        String aa = as.puram("Gokul"," "+ "raj");
+        System.out.println(aa);
+
+    }
+
+}
